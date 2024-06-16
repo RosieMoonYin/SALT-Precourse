a18b17ecc90bfc807834f8ca8ce45bfa08c3105c
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-06-16 19:51:31 +0200

    loops and addresses

--- TrainingGround.Tests/CollectionTests.cs
+++ TrainingGround.Tests/CollectionTests.cs
@@ -1,6 +1,9 @@
 
 
 
+using System.ComponentModel;
+using TrainingGround;
+
 public class CollectionTests
 {
     [Fact]
@@ -33,12 +36,13 @@ public class CollectionTests
     [Fact]
     public void showing_object_initializer()
     {
-        //act
+        //act (this is an object initializer)
         var a = new Address();
         a.Street = "B Street";
         a.StreetNo = 22;
         a.City = "Malmo";
 
+        // (this is also an object initializer)
         var b = new Address
         {
             Street = "B Street",
@@ -53,6 +57,55 @@ public class CollectionTests
 
     }
 
+    [Fact]
+    public void a_list_of_integers_is_very_flexible()
+    {
+        //arrange
+        var integerList = new List<int>();
+        //act
+        integerList.Add(1);
+        integerList.Add(2);
+        integerList.Add(3);
+        integerList.RemoveAt(0);
+        //assert
+        Assert.Equal(2, integerList.Count); //.Count in List == .Length in arrays
+        Assert.Equal(2, integerList[0]);
+        Assert.Equal(3, integerList[1]);
 
+    }
 
+    [Fact]
+    public void lists_can_hold_anything()
+    {
+        //arrage
+        var intList = new List<int>();
+        var stringList = new List<string>();
+        var addressList = new List<Address>();
+        //act
+        intList.Add(1); intList.Add(2); intList.Add(3);
+        stringList.Add("1"); stringList.Add("2");stringList.Add("3");
+        addressList.Add(new Address(){ Street = "Street", StreetNo = 1 }); //these are object initializers
+        addressList.Add(new Address(){ Street = "Street", StreetNo = 2 });
+        addressList.Add(new Address(){ Street = "Street", StreetNo = 3 });
+
+        //assert
+        Assert.IsType<int>(intList[0]);
+        Assert.IsType<string>(stringList[0]);
+        Assert.IsType<Address>(addressList[0]);
+        Assert.IsNotType<int>(addressList[0]);
+    }
+
+    [Fact]
+    public void an_employee_has_more_than_one_address()
+    {
+        //arrange
+        var emp = new Employee("Marcus", "DBCSAS-1253");
+
+        //act
+        emp.Addresses.Add(new Address() { Street = "Work street", StreetNo = 2, City = "Stockholm"});
+        emp.Addresses.Add(new Address() { Street = "Vacation street", StreetNo = 2, City = "Honolulu"});
+
+        //assert
+        Assert.Equal(2, emp.Addresses.Count);
+    }
 }
